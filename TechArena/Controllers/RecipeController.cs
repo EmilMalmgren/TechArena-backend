e6ea83b291f4da88a7769c7b18e4cd6268906682
Emil Malmgren <EmilMa@centiro.se>
2025-05-08 22:11:50 +0200

    Added endpoint for suggest recipe

--- TechArena/Controllers/RecipeController.cs
+++ TechArena/Controllers/RecipeController.cs
@@ -8,45 +8,68 @@ namespace TechArena.Controllers;
 [Route("api/[controller]")]
 public class RecipeController : ControllerBase
 {
-    private readonly RecipeRepository _repository;
+    private readonly FridgeItemRepository _fridgeItemRepository;
+    private readonly RecipeRepository _recipeRepository;
 
-    public RecipeController(RecipeRepository repository)
+    public RecipeController(RecipeRepository repository, FridgeItemRepository fridgeItemRepository)
     {
-        _repository = repository;
+        _recipeRepository = repository;
+        _fridgeItemRepository = fridgeItemRepository;
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetAll() => Ok(await _repository.GetAllAsync());
+    public async Task<IActionResult> GetAll() => Ok(await _recipeRepository.GetAllAsync());
 
     [HttpGet("{id}")]
     public async Task<IActionResult> GetById(string id)
     {
-        var recipe = await _repository.GetByIdAsync(id);
+        var recipe = await _recipeRepository.GetByIdAsync(id);
         return recipe is not null ? Ok(recipe) : NotFound();
     }
 
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] IRecipe recipe)
     {
-        await _repository.CreateAsync(recipe);
+        await _recipeRepository.CreateAsync(recipe);
         return CreatedAtAction(nameof(GetById), new { id = recipe.Id }, recipe);
     }
 
     [HttpPut("{id}")]
     public async Task<IActionResult> Update(string id, [FromBody] IRecipe updatedRecipe)
     {
-        var existing = await _repository.GetByIdAsync(id);
+        var existing = await _recipeRepository.GetByIdAsync(id);
         if (existing is null) return NotFound();
-        await _repository.UpdateAsync(id, updatedRecipe);
+        await _recipeRepository.UpdateAsync(id, updatedRecipe);
         return NoContent();
     }
 
     [HttpDelete("{id}")]
     public async Task<IActionResult> Delete(string id)
     {
-        var existing = await _repository.GetByIdAsync(id);
+        var existing = await _recipeRepository.GetByIdAsync(id);
         if (existing is null) return NotFound();
-        await _repository.DeleteAsync(id);
+        await _recipeRepository.DeleteAsync(id);
         return NoContent();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> SuggestRecipe()
+    {
+        var recipies = await _recipeRepository.GetAllAsync();
+        if (recipies is null) return NotFound();
+        var fridgeItems = await _fridgeItemRepository.GetAllAsync();
+        if (fridgeItems is null) return NotFound();
+
+        foreach (var recipe in recipies)
+        {
+            foreach (var ingredient in recipe.Ingredients)
+            {
+                if(fridgeItems.Any(i => i.Name == ingredient.Name))
+                    recipe.MatchedIngredients.Add(ingredient);              
+                else
+                    recipe.MissingIngredients.Add(ingredient);
+            }
+        }
+        return Ok(recipies);
     }
 }
